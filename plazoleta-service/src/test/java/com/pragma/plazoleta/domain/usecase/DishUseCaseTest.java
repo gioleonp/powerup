697efd53a3c5ecdc4535a9d44c550e1ec969eeb0
Giovanni Leon <giovanni.leonp@pragma.com.co>
2023-03-23 11:34:59 -0500

    4 HU v3: unit tests done

--- plazoleta-service/src/test/java/com/pragma/plazoleta/domain/usecase/DishUseCaseTest.java
+++ plazoleta-service/src/test/java/com/pragma/plazoleta/domain/usecase/DishUseCaseTest.java
@@ -1,17 +1,21 @@
 package com.pragma.plazoleta.domain.usecase;
 
 import com.pragma.plazoleta.domain.api.IRestaurantServicePort;
-import com.pragma.plazoleta.domain.exception.ProprietaryNotMatchException;import com.pragma.plazoleta.domain.model.CategoryModel;
+import com.pragma.plazoleta.domain.exception.ProprietaryNotMatchException;
+import com.pragma.plazoleta.domain.model.CategoryModel;
 import com.pragma.plazoleta.domain.model.DishModel;
 import com.pragma.plazoleta.domain.model.RestaurantModel;
+import com.pragma.plazoleta.domain.model.UserModel;
 import com.pragma.plazoleta.domain.spi.persistence.IDishPersistencePort;
 import com.pragma.plazoleta.domain.spi.persistence.IRestaurantPersistencePort;
 import com.pragma.plazoleta.domain.spi.servicecommunication.IUserServiceCommunicationPort;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;import org.mockito.InjectMocks;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import static org.assertj.core.api.AssertionsForClassTypes.assertThatExceptionOfType;import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatExceptionOfType;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -21,7 +25,7 @@ class DishUseCaseTest {
 
     @Mock IDishPersistencePort dishPersistencePort;
     @Mock IRestaurantServicePort restaurantServicePort;
-    @Mock IUserServiceCommunicationPort serviceCommunicationPort;
+    @Mock IUserServiceCommunicationPort userServiceCommunicationPort;
     @Mock IRestaurantPersistencePort restaurantPersistencePort;
 
     DishModel expectedDishModel =
@@ -34,6 +38,18 @@ class DishUseCaseTest {
                     null,
                     "imagen.url",
                     true);
+
+    RestaurantModel expectedRestaurant =
+            new RestaurantModel(
+                    1L,
+                    "donde keylly",
+                    "piedra de bolivar",
+                    1L,
+                    "573058388527",
+                    "http.foto.com",
+                    "1000000");
+
+    Long idProprietary = 1L;
 
     @Test
     void saveDishOk() {
@@ -64,19 +80,9 @@ class DishUseCaseTest {
     }
 
     @Test
-    void saveDishNotProprietary() {
+    void saveDishNotCorrectProprietary() {
 
         Long idProprietary = 2L;
-
-        RestaurantModel expectedRestaurant =
-                new RestaurantModel(
-                        1L,
-                        "donde keylly",
-                        "piedra de bolivar",
-                        1L,
-                        "573058388527",
-                        "http.foto.com",
-                        "1000000");
 
         // Given
         expectedDishModel.setRestaurante(expectedRestaurant);
@@ -85,8 +91,53 @@ class DishUseCaseTest {
                 .thenReturn(expectedRestaurant);
 
         // Then
-        assertThatExceptionOfType(ProprietaryNotMatchException.class).isThrownBy(() ->
-            underTest.saveDish(idProprietary, expectedDishModel)
-        ).withMessageMatching("USER NOT AUTHORIZED");
+        assertThatExceptionOfType(ProprietaryNotMatchException.class)
+                .isThrownBy(() -> underTest.saveDish(idProprietary, expectedDishModel))
+                .withMessageMatching("USER NOT AUTHORIZED");
+    }
+
+    @Test
+    void UpdateDishOk() {
+        // Given
+        UserModel foundUserModel = new UserModel();
+        foundUserModel.setId(idProprietary);
+
+        expectedDishModel.setRestaurante(expectedRestaurant);
+
+        when(dishPersistencePort.findDishById(expectedDishModel.getId()))
+                .thenReturn(expectedDishModel);
+
+        when(userServiceCommunicationPort.findUserById(idProprietary)).thenReturn(foundUserModel);
+
+        // When
+        underTest.updateDish(idProprietary, expectedDishModel.getId(), expectedDishModel);
+
+        // Then
+        verify(dishPersistencePort).updateDish(expectedDishModel);
+        verify(userServiceCommunicationPort).findUserById(idProprietary);
+    }
+
+    @Test
+    void UpdateDishWithWrongProprietary() {
+        // Given
+        UserModel foundUserModel = new UserModel();
+        foundUserModel.setId(2L);
+
+        expectedDishModel.setRestaurante(expectedRestaurant);
+
+        when(dishPersistencePort.findDishById(expectedDishModel.getId()))
+                .thenReturn(expectedDishModel);
+
+        when(userServiceCommunicationPort.findUserById(idProprietary)).thenReturn(foundUserModel);
+
+        // When
+        assertThatExceptionOfType(ProprietaryNotMatchException.class)
+                .isThrownBy(
+                        () ->
+                                underTest.updateDish(
+                                        idProprietary,
+                                        expectedDishModel.getId(),
+                                        expectedDishModel))
+                .withMessageMatching("USER NOT AUTHORIZED");
     }
 }
