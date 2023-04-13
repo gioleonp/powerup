323b11d82dbed5a322984abd9bf3ae97007e940a
Giovanni Leon <giovanni.leonp@pragma.com.co>
2023-04-13 16:33:14 -0500

    dockerizing microservices and changing local database to RDS service

--- twilio-service/src/test/java/com/pragma/twilio/TwilioApplicationTests.java
+++ twilio-service/src/test/java/com/pragma/twilio/TwilioApplicationTests.java
@@ -1,14 +1,7 @@
 package com.pragma.twilio;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.cloud.config.server.EnableConfigServer;
-
-@EnableConfigServer
-@SpringBootTest
 class TwilioApplicationTests {
 
-    @Test
     void contextLoads() {
     }
 
